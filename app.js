234eab0c6a1035adf8c6ea2c5cd771f4d1997e81
Mbanks1 <michael1banks@icloud.com>
2021-02-18 13:31:08 -0500

    rendering todos

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-document.querySelector('form').addEventListener('submit')
+document.querySelector('form').addEventListener('submit', handleSubmitForm)
+
+
+
+function handleSubmitForm(e){
+    e.preventDefault();
+    let input = document.querySelector('input');
+    if (input.value != '')
+         addTodo(input.value);
+    input.value = ';'
+}
+
+function addTodo(todo) {
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+
+    li.innerHTML = todo;
+
+    li.classList.add('todo-list-item');
+    ul.appendChild(li);
+    console.log(todo)
+}
